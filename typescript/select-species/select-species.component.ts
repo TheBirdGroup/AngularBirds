28e7b34f87716427f192dc446a3447ac1fec0f02
Lars Dølvik <larsdolvik@outlook.com>
2016-04-25 13:22:09 +0200

    Made species directive and ts-file

---
+++ typescript/select-species/select-species.component.ts
@@ -0,0 +1,34 @@
+/**
+ * Created by larsdolvik on 25/04/16.
+ */
+import { Component, EventEmitter, Input, OnInit } from 'angular2/core';
+
+
+
+@Component({
+    selector: 'birdid-quiz-addditional-settings',
+    templateUrl: 'app/media-additional-settings/quiz-additional-settings.component.html',
+    styleUrls:  ['app/media-additional-settings/quiz-additional-settings.component.css'],
+
+    directives: [
+
+    ],
+    providers: [
+
+    ],
+    outputs: ['']
+})
+export class SelectSpeciesComponent implements OnInit {
+
+
+    constructor(){
+    }
+
+
+    ngOnInit() {
+
+    }
+}
+
+
+

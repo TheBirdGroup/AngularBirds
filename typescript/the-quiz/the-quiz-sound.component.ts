d064f34e77b3faef3bc32ba842f13de762da53dc
Jan Greger Hemb <jan.greger@gmail.com>
2016-04-26 12:11:11 +0200

    Added more design and working progress bar to sound component

--- typescript/the-quiz/the-quiz-sound.component.ts
+++ typescript/the-quiz/the-quiz-sound.component.ts
@@ -20,6 +20,13 @@ import { QuizSettingsService }  from './../shared/quiz-settings.service';
 export class TheQuizSoundComponent implements OnInit{
 	title = 'Birdid Quiz TheQuizComponent!';
 	@ViewChild("myAudio") myAudio;
+	@ViewChild("progressBar") progressBar;
+
+	progressPercent = 0;
+	currentTime = 0;
+	totTime = 0;
+
+
 	mediaURLStart = "https://hembstudios.no/birdid/";
 	extraSiteID;
 	soundMiddleURL = ""
@@ -44,30 +51,50 @@ export class TheQuizSoundComponent implements OnInit{
 		this.extraSiteID = "&siteID="+siteID;
 
 	}
-playAudio(){
-	this.myAudio.nativeElement.play();
-	console.log(this.myAudio);
-}
-
-pauseAudio(){
-	this.myAudio.nativeElement.pause();
-}
-
-volumeIncrease(){
-	this.volume+=0.1;
-	if(this.volume>1){
-		this.volume = 1;
+	playAudio(){
+		this.myAudio.nativeElement.play();
+		console.log(this.myAudio);
 	}
-	this.myAudio.nativeElement.volume=this.volume;
-}
 
-volumeDecrease(){
-	this.volume-=0.1;
-	if(this.volume<0){
-		this.volume = 0;
+	pauseAudio(){
+		this.myAudio.nativeElement.pause();
 	}
-	this.myAudio.nativeElement.volume=this.volume;
-}
+
+	volumeIncrease(){
+		this.volume+=0.1;
+		if(this.volume>1){
+			this.volume = 1;
+		}
+		this.myAudio.nativeElement.volume=this.volume;
+	}
+
+	volumeDecrease(){
+		this.volume-=0.1;
+		if(this.volume<0){
+			this.volume = 0;
+		}
+		this.myAudio.nativeElement.volume=this.volume;
+	}
+
+	updateProgressbar(event){
+
+		this.progressPercent = (this.myAudio.nativeElement.currentTime / this.myAudio.nativeElement.duration) * 100;
+
+		this.currentTime = Math.floor(this.myAudio.nativeElement.currentTime);
+		this.totTime = Math.floor(this.myAudio.nativeElement.duration);
+
+		//console.log('tot:',this.myAudio.nativeElement.duration , ' CURRENT: ', this.myAudio.nativeElement.currentTime)
+
+		//console.log(this.progressPercent);
+
+	}
+
+	soundLoadedEvent(event){
+
+		this.currentTime = Math.floor(this.myAudio.nativeElement.currentTime);
+		this.totTime = Math.floor(this.myAudio.nativeElement.duration);
+
+	}
 
 
 

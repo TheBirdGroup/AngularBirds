e20871abb852778c6cf8d16f5f145acc8c920504
Jan Greger Hemb <jan.greger@gmail.com>
2016-04-28 11:58:00 +0200

    Added several other datapoints to the quiz question class.

--- typescript/the-quiz/the-quiz-question.class.ts
+++ typescript/the-quiz/the-quiz-question.class.ts
@@ -3,6 +3,10 @@ export class QuizQuestion {
 	private rightAnswers = [];
 	private choices = [];
 
+	private mediaIds = [];
+	private mediaSources = [];
+	private extraInfoes = [];
+
 	constructor(){
 
 	}
@@ -17,6 +21,55 @@ export class QuizQuestion {
 	addChoice(id, name, latin){
 
 		this.choices.push({'id': id, 'name': name, 'latin': latin});
+
+	}
+
+	addMediaSource(id, mediaUrl){
+
+		this.mediaSources.push({'id': id, 'mediaUrl': mediaUrl});
+
+	}
+
+	addExtraInfo(text){
+
+		this.extraInfoes.push({'extraInfo': text});
+
+	}
+
+	addMediaIds(id){
+
+		this.mediaIds.push({'id': id});
+
+	}
+
+	getMediaIds(){
+
+		return this.mediaIds;
+
+	}
+
+	//returns as one string of all extra infoes
+	getExtraInfo():string{
+
+		let returnString =  " ";
+
+		for (let currentID of Object.keys(this.extraInfoes)) {
+			if(this.extraInfoes[currentID].text.length > 0){
+				returnString += this.extraInfoes[currentID].text + ",";
+			}
+		}
+
+		if(returnString.length == 0){
+			return ""
+		}else{
+			return returnString.substring(0, returnString.length-1);;
+		}
+
+	}
+
+	getMediaSourses(){
+
+		return this.mediaSources;
 
 	}
 

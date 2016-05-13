095ac2e1b10e906b25631e9c865dc7cb4c52dd22
Jan Greger Hemb <jan.greger@gmail.com>
2016-05-13 21:40:02 +0200

    Fixed indeting and some variable use

--- typescript/shared/quiz-login.service.ts
+++ typescript/shared/quiz-login.service.ts
@@ -11,29 +11,29 @@ export class QuizLoginService{
 
 constructor(private _http: Http){}
 
-theAction;
-
     Login(mail,password,autoLogin,action){ // this is also for registering, the naming is horrible
+
         let theMail = mail;
         let thePassword = password;
-        if (autoLogin==null){
-            autoLogin=false;
+        if (autoLogin == null){
+            autoLogin = false;
         }else{
-        let theAutoLogin = autoLogin;
-            }
-        this.theAction=action;
+            let theAutoLogin = autoLogin;
+        }
 
-         let body = "email=" + theMail;
-         body += "&password=" + thePassword;
-         body += "&autologin=" + autoLogin;
+        let theAction = action;
+
+        let body = "email=" + theMail;
+        body += "&password=" + thePassword;
+        body += "&autologin=" + autoLogin;
 
         var headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded');
-            return this._http.post('https://hembstudios.no/birdid/bird/loginControlAJAX.php?action='+this.theAction, body,{
+
+        return this._http.post('https://hembstudios.no/birdid/bird/loginControlAJAX.php?action='+theAction, body,{
             headers: headers
-            })
-            .map(response => response.json());
-        }
+        }).map(response => response.json());
+    }
 
 
 
